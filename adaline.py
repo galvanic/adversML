76b99489bcc9484cf4c5dc1a596eb8cd3cadfa13
galvanic <justinecourty@yahoo.fr>
2016-07-16 17:41:54 +0100

    move termination condition helper functions to seperate file

--- adaline.py
+++ adaline.py
@@ -18,20 +18,8 @@ TODO clean up the code further, especially duplicated sections (adaline model
 import sys
 import numpy as np
 import numpy.random as rand
+from gradientdescent import max_iters
 from performance import get_cost, get_error
-
-### Helper functions for termination conditions
-
-def Counter(max_iterations):
-    i = max_iterations
-    while True:
-        yield i
-        i -= 1
-
-def max_iters(max_iterations):
-    '''Assumes max_iterations is a Natural Integer.'''
-    counter = Counter(max_iterations)
-    return lambda: not next(counter)
 
 
 def train_adaline(features, labels,
--- gradientdescent.py
+++ gradientdescent.py
@@ -1,2 +1,17 @@
 
 from __future__ import division
+
+
+### Helper functions for termination conditions
+
+def Counter(max_iterations):
+    i = max_iterations
+    while True:
+        yield i
+        i -= 1
+
+def max_iters(max_iterations):
+    '''Assumes max_iterations is a Natural Integer.'''
+    counter = Counter(max_iterations)
+    return lambda: not next(counter)
+
