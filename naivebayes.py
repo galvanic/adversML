b576c043885aa9eed20766339d60c42cb55e138e
galvanic <justinecourty@yahoo.fr>
2016-07-16 16:45:05 +0100

    rename _ham to ham

--- naivebayes.py
+++ naivebayes.py
@@ -46,29 +46,29 @@ def train_naivebayes(features, labels):
     '''
     ## setup
     X, Y = features, labels
-    _ham_label = 0
+    ham_label = 0
     spam_label = 1
     N, D = X.shape    ## number of N: training samples, D: features
     tolerance = 1e-30 ## tolerance factor (to avoid under/overflows)
 
     ## estimate prior probability of spam class
     prior_spam = np.sum(Y == spam_label) / N
-    prior__ham = 1 - prior_spam
+    prior_ham  = 1 - prior_spam
 
-    indices__ham = np.nonzero(Y == _ham_label)[0]
+    indices_ham  = np.nonzero(Y ==  ham_label)[0]
     indices_spam = np.nonzero(Y == spam_label)[0]
-    N__ham = len(indices__ham)
+    N_ham  = len(indices_ham)
     N_spam = len(indices_spam)
 
     ## estimate likelihood parameters for each class
     ## looks at presence of features in each class
-    likeli__ham = np.sum(X[indices__ham], axis=0) / N__ham
+    likeli_ham  = np.sum(X[indices_ham],  axis=0) / N_ham
     likeli_spam = np.sum(X[indices_spam], axis=0) / N_spam
 
-    likeli__ham, likeli_spam = map(lambda p: p.reshape((D, 1)), [likeli__ham, likeli_spam])
-    likeli__ham, likeli_spam = map(process_parameters, [likeli__ham, likeli_spam])
+    likeli_ham, likeli_spam = map(lambda p: p.reshape((D, 1)), [likeli_ham, likeli_spam])
+    likeli_ham, likeli_spam = map(process_parameters, [likeli_ham, likeli_spam])
 
-    return prior__ham, prior_spam, likeli__ham, likeli_spam
+    return prior_ham, prior_spam, likeli_ham, likeli_spam
 
 
 def test_naivebayes(parameters, features):
@@ -83,15 +83,15 @@ def test_naivebayes(parameters, features):
     - predicted: labels
     '''
     ## notation
-    X, prior__ham, prior_spam, likeli__ham, likeli_spam = features, *parameters
+    X, prior_ham, prior_spam, likeli_ham, likeli_spam = features, *parameters
     N, D = X.shape
 
     ## apply model
     ## Bernouilli Naive Bayes, takes into account absence of a feature
     ## TODO figure out why log of posterior calculation is this
-    log_posterior__ham = np.log(prior__ham) +                   \
-                         np.dot(   X,  np.log(  likeli__ham)) + \
-                         np.dot((1-X), np.log(1-likeli__ham))
+    log_posterior_ham  = np.log(prior_ham) +                    \
+                         np.dot(   X,  np.log(  likeli_ham)) +  \
+                         np.dot((1-X), np.log(1-likeli_ham))
     log_posterior_spam = np.log(prior_spam)   +                 \
                          np.dot(   X,  np.log(  likeli_spam)) + \
                          np.dot((1-X), np.log(1-likeli_spam))
@@ -99,12 +99,11 @@ def test_naivebayes(parameters, features):
     ## no need to normalise since we are just interested in which
     ## posterior is higher (ie. which label is most likely given the data)
 
-    log_posterior__ham, log_posterior_spam = map(np.ravel,
-        [log_posterior__ham, log_posterior_spam])
+    log_posterior_ham, log_posterior_spam = map(np.ravel, [log_posterior_ham, log_posterior_spam])
     ## calculate output
     ## assign class which is most likely over the other
     ## this works because labels are 0 and 1 for ham and spam respectively
-    predicted = (log_posterior_spam > log_posterior__ham)
+    predicted = (log_posterior_spam > log_posterior_ham)
 
     return predicted
 
