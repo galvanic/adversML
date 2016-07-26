5a2d5864a7d13b3eea4e04e90c2621a8a3d72da0
galvanic <justinecourty@yahoo.fr>
2016-07-26 12:36:30 +0100

    implement saving the dataframe as a pickle and string

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from __future__ import division
 '''
 '''
 import sys
+import os
+import time
 
 from helpers.pipeline import perform_experiment_batch
 
 
+def change_name(old, new, lst):
+    names = [new if (x == old) else x for x in lst]
+    return names
 
-def main():
+
+def main(outfolder):
 
     ## put iteration last, but other dimensions is preference only
     ## TODO use OrderedDict
@@ -33,9 +39,25 @@ def main():
 
     df = perform_experiment_batch(parameter_ranges, fixed_parameters)
 
+    ## save the data
+    saved_at = time.strftime('%y%m%d%H%M', time.localtime(time.time()))
+    outfilepath = os.path.join(outfolder, saved_at)
+    df.to_pickle('%s-df.dat' % outfilepath)
+
+    ## save also as string for human readability
+    ## change column and index names for clarity
+    new_names = change_name('percentage_samples_poisoned', '% poisoned', df.index.names)
+    df.index = df.index.set_names(new_names)
+
+
+    with open('%s-df.txt' % outfilepath, 'w') as outfile:
+        outfile.write(df.to_string(col_space=8, float_format='%.3f'))
+
     return df
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+
+    outfolder = sys.argv[1] if len(sys.argv) > 1 else '.'
+    sys.exit(main(outfolder))
 
