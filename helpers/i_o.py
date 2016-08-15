38750a1ee709b3c50754c7adbb208cb56e973c9d
galvanic <justinecourty@yahoo.fr>
2016-08-15 23:39:48 +0100

    join both config files (ranges and defaults) into one file
    makes sense to put them into one so that it can be edited more easily and saved along with the results

--- helpers/i_o.py
+++ helpers/i_o.py
@@ -4,6 +4,7 @@ from __future__ import division
 '''
 import os
 import time
+import shutil
 import pandas as pd
 
 import logging
@@ -18,7 +19,7 @@ def get_time_id(time_format='%y%m%d%H%M'):
     return time_id
 
 
-def save_df(df, outfolder, experiment_id=None):
+def save_df(df, outfolder, experiment_id=None, specs_filepath=None):
     '''
     '''
 
@@ -27,11 +28,15 @@ def save_df(df, outfolder, experiment_id=None):
     df.to_pickle('%s-df.dat' % outfilepath)
     logger.info('Saved to %s\n' % outfilepath)
 
+    if specs_filepath:
+        experiment_purpose = os.path.basename(specs_filepath)[-5:]
+        shutil.move(specs_filepath, '%s-%s.yaml' % (outfilepath, experiment_purpose))
+
     ## save also as string for human readability
     string = df.to_string(col_space=8, float_format=lambda x: '%.2f' % x)
     logger.info('\n%s' % string)
 
-    return
+    return string
 
 
 def join_repetitions(ifilepaths):
--- main.py
+++ main.py
@@ -13,41 +13,39 @@ from helpers.batch import perform_experiment_batch
 from helpers.i_o import get_time_id, save_df
 
 
-def main(parameter_ranges_filepath, infolder, outfolder,
-        fixed_parameters_filepath='config/default_spec.yaml',
-        num_threads=8):
+def main(batch_specs_filepath, infolder, outfolder, num_threads=1):
     '''
     '''
 
     ## load data
-    with open(fixed_parameters_filepath, 'r') as infile:
-        fixed_parameters = yaml.safe_load(infile)
+    with open(batch_specs_filepath, 'r') as infile:
+        batch_specs = yaml.safe_load(infile)
+
+    fixed_parameters = batch_specs['fixed_parameters']
+    parameter_ranges = batch_specs['parameter_ranges']
 
     batch_id = str(get_time_id())
     fixed_parameters['batch_id'] = batch_id
+
     LOGGING_CONFIG['handlers']['file']['filename'] = './%s.log' % batch_id
     logging.config.dictConfig(LOGGING_CONFIG)
-
-    with open(parameter_ranges_filepath, 'r') as infile:
-        parameter_ranges = yaml.safe_load(infile)
-
 
     ## carry out experiments
     df = perform_experiment_batch(parameter_ranges, fixed_parameters, infolder,
         num_threads=num_threads)
 
     ## save results
-    save_df(df, outfolder, batch_id)
+    save_df(df, outfolder, batch_id, batch_specs_filepath)
 
     return
 
 
 if __name__ == '__main__':
 
-    parameter_ranges_filepath = sys.argv[1] if len(sys.argv) > 1 else 'config/example.yaml'
+    batch_specs_filepath = sys.argv[1] if len(sys.argv) > 1 else 'config/example.yaml'
     infolder = sys.argv[2] if len(sys.argv) > 2 else '../datasets/processed'
     outfolder = sys.argv[3] if len(sys.argv) > 3 else '.'
     num_threads = int(sys.argv[4]) if len(sys.argv) > 4 else None
 
-    sys.exit(main(parameter_ranges_filepath, infolder, outfolder, num_threads=num_threads))
+    sys.exit(main(batch_specs_filepath, infolder, outfolder, num_threads=num_threads))
 
