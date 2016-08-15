c5a228a8ee7aa2286b08e6b1782a909547c2d92c
galvanic <justinecourty@yahoo.fr>
2016-08-15 13:00:16 +0100

    factor out the gradient descent method
    clearer code
    
    also:
    - hack so that smaller batch is discarded but still same number of
    weight updates (which I refer to as epochs)

--- helpers/gradientdescent.py
+++ helpers/gradientdescent.py
@@ -8,6 +8,9 @@ import numpy as np
 from helpers.logging import tls, log
 from helpers.performance import get_error
 
+class TooSmallBatchException(Exception):
+    pass
+
 
 def get_cost(Y, O):
     '''Calculate cost using Means Squared'''
@@ -15,6 +18,57 @@ def get_cost(Y, O):
     cost = np.mean(np.square(Y - O))
     return cost
 
+
+###
+### Gradient descent methods functions
+###
+
+@log
+def stochastic(X, Y, permuted_indices, epoch, **kwargs):
+    '''
+    '''
+    N = X.shape[0]
+
+    ## stochastic GD: only update using 1 sample
+    sample = permuted_indices[epoch % N]
+    tls.logger.debug('- sample (%d/%d): %d' % (epoch % N, N, sample))
+    x, y = X[sample].reshape(1, D), Y[sample]
+    return x, y
+
+
+@log
+def mini_batch(X, Y, permuted_indices, epoch, batch_size):
+    '''
+    '''
+    N = X.shape[0]
+
+    start = (epoch * batch_size) % N
+    end = (epoch * batch_size + batch_size) % N or None
+    samples = permuted_indices[start:end]
+
+    #end = end if (end and end > start) else None
+    if (end and end < start): ## do not train on smaller batch at the end
+        raise TooSmallBatchException
+    tls.logger.debug('- samples (%d-%s/%d): %s' % (start, end, N, samples))
+    x, y = X[samples], Y[samples]
+    return x, y
+
+
+def batch(X, Y, **kwargs):
+    '''
+    '''
+    return X, Y
+
+
+GD_METHODS = {
+    'stochastic': stochastic,
+    'mini-batch': mini_batch,
+    'batch': batch,
+}
+
+###
+###
+###
 
 @log
 def gradient_descent(features, labels,
@@ -32,7 +86,7 @@ def gradient_descent(features, labels,
     '''
     tls.logger.info('learning rate: %f' % learning_rate)
     tls.logger.info('using %s' % gradient_descent_method)
-
+    get_batch = GD_METHODS[gradient_descent_method]
 
     ## notation
     X, Y = features, labels
@@ -44,7 +98,9 @@ def gradient_descent(features, labels,
     tls.logger.debug('initial weights: %s' % np.ravel(W))
 
     ## evaluate the termination condition
-    for epoch in range(max_epochs):
+    epoch = 0
+    while epoch < max_epochs:
+    #for epoch in range(max_epochs):
         tls.logger.info('epoch %d:' % epoch)
 
         ## mix up samples (they will therefore be fed in different order at
@@ -53,30 +109,13 @@ def gradient_descent(features, labels,
         if epoch % N == 0:
             permuted_indices = np.random.permutation(N)
 
-        ## TODO implement GD method as different function based on argument
-        if gradient_descent_method == 'stochastic':
-            ## stochastic GD: only update using 1 sample
-            sample = permuted_indices[epoch % N]
-            tls.logger.debug('- sample (%d/%d): %d' % (epoch % N, N, sample))
-            x, y = X[sample].reshape(1, D), Y[sample]
-
-        elif gradient_descent_method == 'mini-batch':
-            start = (epoch * batch_size) % N
-            end = (epoch * batch_size + batch_size) % N or None
-            samples = permuted_indices[start:end]
-
-            #end = end if (end and end > start) else None
-            if (end and end < start): ## do not train on smaller batch at the end
-                tls.logger.debug('New pass through dataset')
-                continue
-            tls.logger.debug('- samples (%d-%s/%d): %s' % (start, end, N, samples))
-            x, y = X[samples], Y[samples]
-
-        elif gradient_descent_method == 'batch':
-            x, y = X, Y
-
-        else: ## batch
-            x, y = X, Y
+        try:
+            x, y = get_batch(X, Y, permuted_indices, epoch, batch_size)
+        except TooSmallBatchException:
+            tls.logger.info('New pass through dataset')
+            epoch += 1
+            max_epochs += 1 ## to keep same number of weight updates
+            continue
 
         tls.logger.debug('- x: (%s, %s)' % x.shape)
         tls.logger.debug('- y: %s' % str(y.shape))
@@ -100,6 +139,8 @@ def gradient_descent(features, labels,
         cost = get_cost(Y, P)
         tls.logger.info('- cost = %.2e' % cost)
         tls.logger.info('- error = %.2f' % error)
+
+        epoch += 1
 
     return W
 
