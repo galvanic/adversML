7d0536299474b568c9e9ddb28c1fa13cefd01eed
galvanic <justinecourty@yahoo.fr>
2016-08-16 23:12:11 +0100

    update one liners

--- oneliners.sh
+++ oneliners.sh
@@ -5,10 +5,11 @@
 
 ## TODO turn into functions
 
-LOG_FILEPATH=./1608151500.log
+LOG_FILEPATH=./1608161201.log
 export LOG_FILEPATH
 
-## experiment number,number of epochs, error at end (to get idea of convergence) - only works if experiments were run serially, not using threads
+## experiment number,number of epochs, error at end (to get idea of convergence)
+## /!\ only works if experiments were run serially, not using threads /!\
 cat $LOG_FILEPATH | grep -P -e 'gradientdescent| performance' | grep -P -e '(error|epoch|INFO: performance).*' -o | grep -o -P -e '\d\.?\d{0,2}|: perf' | xargs -n2 |  grep perf -B 1 | xargs -n3 -d'\n' | cut -d' ' -f1-2 | sed 's/ /\t/g' | awk '{print NR-1 "\t" $0}'
 
 ## overview of experiments run and still running
@@ -20,5 +21,12 @@ cat $LOG_FILEPATH | grep -o -P -e '_\d{1,3}+' | sed 's/_//g' | sort | uniq -c | 
 
 ## get epoch, cost and error for one experiment
 EXPERIMENT_NUM=0
-#cat $LOG_FILEPATH 2>/dev/null | grep "_$EXPERIMENT_NUM]" 2>/dev/null | grep -P -e 'cost|error' 2>/dev/null | cut -d'=' -f2 | xargs -n2 | cat -n
+cat $LOG_FILEPATH 2>/dev/null | grep "_$EXPERIMENT_NUM]" 2>/dev/null | grep -P -e 'cost|error' 2>/dev/null | cut -d'=' -f2 | xargs -n2 | cat -n
 
+## to start debugging a certain experiment
+EXPERIMENT_NUM=0
+cat 1608161201.log | grep -P -e "experiment #\d{10}_$EXPERIMENT_NUM\]" | cut -c69-174 | less
+
+## get results with experiment numbers and a pattern highlighted to delimit parts visually
+PATTERN=0.5
+cat $LOG_FILEPATH | grep metrics -A 100000 |  awk '{print NR-3 "\t" $0}' | sed 's/-[0-9]\b/  /g' | grep -P -e "$PATTERN|$"
